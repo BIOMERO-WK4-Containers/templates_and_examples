1fdf9d110ec97d3e528ba689897f6793e6803293
Vladimir Ulman (@Karolina) <vladimir.ulman@vsb.cz>
2025-09-22 12:33:30 +0200

    FIX,COS: clean_corners.py adapted paths for Karolina setup

--- aux_processing/after_cp4_segmentation/clean_corners.py
+++ aux_processing/after_cp4_segmentation/clean_corners.py
@@ -1,15 +1,18 @@
 import tifffile as TIFF
 
+in_folder  = '/mnt/proj2/dd-24-22/Mastodon_paper/orig_as_CTC__1px_markers/SEG_LZWed'
+out_folder = '/mnt/proj2/dd-24-22/Mastodon_paper/orig_as_CTC__own_SEG_TRA/SEG'
+
 cw = 170
 for tp in range(0,601):
 #for tp in range(245,601,5):
     print("doing tp:",tp)
-    i = TIFF.imread(f"/home/ulman/papers/mastodonPaper/E1_cellposev4_trackatra/SEG_LZWed/mask{tp:03}.tif")
+    i = TIFF.imread(f"{in_folder}/mask{tp:03}.tif")
 
     i[:, 0:cw,        0:cw+10] = 0  # TL
     i[:, 0:cw,         -cw:-1] = 0  # TR
     i[:, -cw-10:-1,   0:cw+30] = 0  # BL
     i[:, -cw-10:-1, -cw-10:-1] = 0  # BR
 
-    TIFF.imwrite(f"/home/ulman/papers/mastodonPaper/E1_cellposev4_trackatra/SEG_LZWed_cleanedCorners/mask{tp:03}.tif",i)
+    TIFF.imwrite(f"{out_folder}/mask{tp:03}.tif",i)
 
