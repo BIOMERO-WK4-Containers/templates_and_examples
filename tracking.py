851021c4f570dcc28cd925dc0f6d98a16f7a83b2
Vladimír Ulman <ulman@fi.muni.cz>
2025-09-23 15:43:40 +0200

    CHG: tracking loader uses multithreading

--- tracking.py
+++ tracking.py
@@ -5,6 +5,7 @@ from trackastra.tracking import graph_to_ctc, graph_to_napari_tracks
 from trackastra.data import example_data_bacteria
 import tifffile as TIFF
 from skimage.transform import resize as img_resize
+import util.parallelism_paradigms as P
 
 device = "automatic" # explicit choices: [cuda, mps, cpu]
 
@@ -64,10 +65,46 @@ def load_ctc(from_folder, tp_range_from, tp_range_till):
     return imgs,masks
 
 
+## <  parallel version of load_ctc() >
+def load_ctc_worker(input_tuple):
+    imgs,idx,path,is_mask = input_tuple
+    print("reading index :",idx)
+    imgs[idx] = read_and_downscale(path, is_mask=is_mask)
+    return "OK" # flag the job is done
+
+NUM_PARALLEL_WORKERS = 50
+def load_ctc_with_multiprocessing(from_folder, tp_range_from, tp_range_till):
+    global NUM_PARALLEL_WORKERS
+
+    # load the first image to understand the shape
+    fp = f"{from_folder}/t{tp_range_from:03}.tif"
+    print("reading the first  raw file:",fp)
+    i = read_and_downscale(fp, is_mask=False)
+
+    tp_range = tp_range_till - tp_range_from +1
+    imgs = np.zeros([tp_range,*i.shape], dtype=i.dtype)
+    masks = np.zeros([tp_range,*i.shape], dtype='uint16')
+    print("allocated memory for twice the shapes: ",imgs.shape)
+    print("...this is likely as much as ",len(imgs.flat)*4.0 / float(1 << 30),"GB")
+    imgs[0] = i
+
+    tasks = [ (imgs,tp-tp_range_from,f"{from_folder}/t{tp:03}.tif",False) for tp in range(tp_range_from+1, tp_range_till+1) ]
+    P.process_with_multithreading(tasks, load_ctc_worker, NUM_PARALLEL_WORKERS, "ctc_loader")
+
+    print("reading the masks now")
+    tasks = [ (masks,tp-tp_range_from,f"{from_folder}/SEG/mask{tp:03}.tif",True) for tp in range(tp_range_from, tp_range_till+1) ]
+    P.process_with_multithreading(tasks, load_ctc_worker, NUM_PARALLEL_WORKERS, "ctc_loader")
+
+    print("done reading.")
+    return imgs,masks
+## </ parallel version of load_ctc() >
+
+
 # load some test data images and masks
 # imgs, masks = example_data_bacteria()
 
-imgs, masks = load_ctc('/mnt/proj2/dd-24-22/Mastodon_paper/orig_as_CTC__own_SEG_TRA/',0,600)
+#imgs, masks = load_ctc('/mnt/proj2/dd-24-22/Mastodon_paper/orig_as_CTC__own_SEG_TRA/',0,600)
+imgs, masks = load_ctc_with_multiprocessing('/mnt/proj2/dd-24-22/Mastodon_paper/orig_as_CTC__own_SEG_TRA/',0,600)
 
 # Load a pretrained model
 #model = Trackastra.from_pretrained("general_2d", device=device)
