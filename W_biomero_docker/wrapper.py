2488373bacb735ee5e063bdf289704189cbaaa93
Vladimír Ulman <ulman@fi.muni.cz>
2025-10-28 14:21:04 +0900

    FIX->DEMO: the wrapper.py in the Docker to use the example image processing

--- W_biomero_docker/wrapper.py
+++ W_biomero_docker/wrapper.py
@@ -1,6 +1,19 @@
-# test import
 import numpy as np
 
-# test "being executed"
-print("hello from inside")
+
+def biomero_entry_point(args):
+
+    from image_processing import MyAwesomeImgProcessing
+    imgproc = MyAwesomeImgProcessing(2,3)
+
+    # loop here
+    img = np.zeros((30,20))
+    imgproc.execute(img)
+    imgproc.execute(img)
+
+    imgproc.release_resources()
+
+
+if __name__ == '__main__':
+    biomero_entry_point([])
 
