f62ae1ea52d3560397e95226c128c94fd1c59519
Vladimír Ulman <ulman@fi.muni.cz>
2025-10-29 15:08:24 +0900

    CHG: W_ex/image_processing example changed a lot:
    - the prototype classes were dropped, instead it is assumed
      that the workhorse script just takes a list of objects
      (a heterogeneous list) and returns again a list of objects...
    
    - the CLI facade, to test locally and directly with a Python,
      is taken out into a separate file "cli_facade_example.py";
      and this facade file uses 'click' tool and does even some
      basic input parameters checking etc.

--- W_example/image_processing.py
+++ W_example/image_processing.py
@@ -1,42 +1,58 @@
-import sys
-sys.path.append("../W_prototypes")
-from prototypes import PrototypeImgToImg
 import numpy as np
+# TODO: add type declarations
 
-class MyAwesomeImgProcessing(PrototypeImgToImg):
-
-    # TODO: add types
-    def __init__(self, param1, param2):
-        self.param1 = param1
-        self.param2 = param2
-
-        print(f"initiating with param1 = {param1}")
-        print(f"initiating with param2 = {param2}")
+# initial default values
+my_param1 = 1
+my_param2 = 2
 
 
-    # TODO: add types for numpy-like arrays
-    def execute(self, img):
-        print(f"doing fake work on an image of size: {img.shape}, and using params {self.param1}, {self.param2}")
-        return img
+def initiate_resources(param1, param2):
+    """
+    Example of (optional!) special function to one-time(!) pre-allocate
+    resources so that the main workhorse code 'process()' need to do it...
+
+    In this lame example, two parameters are taken and memorized to show
+    one way of passing them for later 'process()'-ing.
+    """
+    global my_param1, my_param2
+    my_param1 = param1
+    my_param2 = param2
+
+    print(f"initiating with param1 = {param1}")
+    print(f"initiating with param2 = {param2}")
 
 
-    def release_resources(self):
-        print("just reporting that I'm now releasing my resources")
+def process(input_tuple):
+    """
+    Example function that processes one processing unit of input.
+
+    This "unit of input" can be a single image, but it is not limited to it and
+    the unit can very well be a list/tuple, e.g., [image,aux_info,specific_params]
+    where 'image' is the image to be processed taking into account additional
+    (non-image?) data 'aux_info' with processing tailoring 'specific_params',
+    which is specific to this data pair ('image'+'aux_info'). Another example
+    could be a pair of [raw_image,mask_image] to collect average intensity
+    in each mask, for example.
+
+    Similarly, a list of (heterogenous) items should be returned as a result
+    of the processing. The output list can also be empty '[]'.
+
+    In this lame example, the code is assuming that a single image
+    is provided, and that image is returned back as a fake result.
+    Notice the un-/wrapping with lists.
+    """
+
+    # unpack the image from the input tuple
+    img = input_tuple[0]
+
+    # process the image
+    print(f"doing fake work on an image of size: {img.shape}, and using params {my_param1}, {my_param2}")
+    m = np.max(img) # just to touch the image pixels for the sake of fun
+
+    # return the output as a list of results
+    return [img]
 
 
-import click
-@click.command()
-@click.option('--param1', default=1, help='Info for param1')
-@click.option('--param2', help='Foo and blah for param2')
-def cli_wrapper(param1,param2):
-    imgproc = MyAwesomeImgProcessing(param1,param2)
-
-    img = np.zeros((10,10))
-    imgproc.execute(img)
-    imgproc.execute(img)
-    # no point for releasing when this process code is going to an end anyway
-
-
-if __name__ == '__main__':
-    cli_wrapper()
+def release_resources():
+    print("just reporting that I'm now releasing my resources")
 
